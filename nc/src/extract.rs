de982bfe198cbf900c58d16c390a8f51047dd765
George-Tsiamasiotis <gt.develop@protonmail.com>
2025-07-07 18:13:12 +0300

    feat: added 1D variable extraction methods
        feat: added 'psi_wall' calculation
        maint: added temporary Display impl for `Scalars`

--- nc/src/extract.rs
+++ nc/src/extract.rs
@@ -1,13 +1,18 @@
 use crate::NcError;
 
+fn extract_variable<'a>(
+    f: &'a netcdf::File,
+    name: &'a str,
+) -> Result<netcdf::Variable<'a>, NcError> {
+    f.variable(name)
+        .ok_or(NcError::VariableNotFound(name.into()))
+}
+
 /// Extracts a scalar (0D) variable.
 pub(crate) fn extract_scalar(f: &netcdf::File, name: &str) -> Result<f64, NcError> {
     use crate::NcError::*;
 
-    let var = match f.variable(name) {
-        Some(var) => var,
-        None => return Err(VariableNotFound(name.into())),
-    };
+    let var = extract_variable(f, name)?;
 
     // `.len()` works for N-dimensional arrays too; it returns the total number of elements.
     match var.len() {
@@ -20,6 +25,24 @@ pub(crate) fn extract_scalar(f: &netcdf::File, name: &str) -> Result<f64, NcErro
         Ok(value) => Ok(value),
         Err(dim_err) => Err(DimensionError {
             source: dim_err, // Error::DimensionMismatch
+            field: name.into(),
+        }),
+    }
+}
+
+pub(crate) fn extract_1d(f: &netcdf::File, name: &str) -> Result<Vec<f64>, NcError> {
+    use crate::NcError::*;
+
+    let var = extract_variable(f, name)?;
+
+    if var.len() == 0 {
+        return Err(EmptyVariable(name.into()));
+    }
+
+    match var.get_values::<f64, _>(..) {
+        Ok(value) => Ok(value),
+        Err(err) => Err(DimensionError {
+            source: err, // Error::DimensionMismatch
             field: name.into(),
         }),
     }
--- nc/src/scalars.rs
+++ nc/src/scalars.rs
@@ -1,6 +1,6 @@
 //! Representation of an equilibrium's scalar values.
 
-use crate::{NcError, extract::extract_scalar};
+use crate::{NcError, extract::extract_1d, extract::extract_scalar};
 
 #[derive(Debug)]
 pub struct Scalars {
@@ -14,12 +14,29 @@ impl Scalars {
     pub(crate) fn build(f: &netcdf::File) -> Result<Self, NcError> {
         let baxis = extract_scalar(f, "Baxis")?;
         let raxis = extract_scalar(f, "raxis")?;
-        let psi_wall = 1.0; // TODO:
+        let psis = extract_1d(f, "psi")?;
+
+        // We can safely assume that the coords are sorted.
+        // Whether the variable is empty or not is checked in the extraction.
+        let psi_wall = match psis.last() {
+            Some(psi_wall) => *psi_wall,
+            None => unreachable!(),
+        };
 
         Ok(Self {
             baxis,
             raxis,
             psi_wall,
         })
+    }
+}
+
+impl std::fmt::Display for Scalars {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "Scalars:\n\tbaxis = {:.5} [T],\n\traxis = {:.5} [m],\n\tpsi_wall = {:.5}",
+            self.baxis, self.raxis, self.psi_wall
+        )
     }
 }
